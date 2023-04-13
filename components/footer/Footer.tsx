9b048306167040d77765a08ff93a993665bb8911
lucasnull <lucasnull@live.com>
2023-04-13 11:27:48 -0300

    feat: desktop footer

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -3,9 +3,16 @@ import Icon, {
 } from "deco-sites/fashion/components/ui/Icon.tsx";
 import Text from "deco-sites/fashion/components/ui/Text.tsx";
 import Container from "deco-sites/fashion/components/ui/Container.tsx";
+import type { Image as LiveImage } from "deco-sites/std/components/types.ts";
+import Image from "deco-sites/std/components/Image.tsx";
 
 import Newsletter from "./Newsletter.tsx";
+import type { Props as NewsletterProps } from "./Newsletter.tsx";
 import type { ComponentChildren } from "preact";
+import {
+  Picture,
+  Source,
+} from "https://denopkg.com/deco-sites/std@1.0.0-rc.13/components/Picture.tsx";
 
 export type IconItem = { icon: AvailableIcons };
 export type StringItem = {
@@ -26,17 +33,15 @@ const isIcon = (item: Item): item is IconItem =>
 
 function SectionItem({ item }: { item: Item }) {
   return (
-    <Text variant="caption" tone="default-inverse">
+    <Text variant="caption" tone="default">
       {isIcon(item)
         ? (
-          <div class="border-default border-1 py-1.5 px-2.5">
-            <Icon
-              id={item.icon}
-              width={25}
-              height={20}
-              strokeWidth={0.01}
-            />
-          </div>
+          <Icon
+            id={item.icon}
+            width={20}
+            height={15}
+            strokeWidth={1}
+          />
         )
         : (
           <a href={item.href}>
@@ -57,136 +62,202 @@ function FooterContainer(
 }
 
 export interface Props {
-  sections?: Section[];
+  links: Array<{
+    title: string;
+    items: Array<{
+      icon: AvailableIcons;
+      title: string;
+      href: string;
+    }>;
+  }>;
+  support: {
+    phoneNumber: string;
+    openingHours: string[];
+  };
+  social: Array<{
+    icon: AvailableIcons;
+    href: string;
+  }>;
+  copyright: {
+    logo: LiveImage;
+    lines: string[];
+  };
 }
 
-function Footer({ sections = [] }: Props) {
+function Footer({ links = [], support, social = [], copyright }: Props) {
   return (
-    <footer class="w-full bg-footer flex flex-col divide-y-1 divide-default">
-      <div>
-        <Container class="w-full flex flex-col divide-y-1 divide-default">
-          <FooterContainer class="w-full bg-[#F0E6E6] px-0">
-            <Newsletter
-              icon={"teste"}
-              title={"Inscreva-se e receba nossas promoções e novidades exclusivas"}
-              button={"Enviar"}
-              placeholders={{
-                name: "Primeiro nome",
-                email: "Seu e-mail",
-              }}
-            />
-          </FooterContainer>
+    <footer class="w-full flex flex-col divide-y-1 divide-gray-50">
+      <FooterContainer class="w-full bg-[#F0E6E6]">
+        <Newsletter
+          icon={""}
+          title={"Inscreva-se e receba nossas promoções e novidades exclusivas"}
+          button={"Enviar"}
+          placeholders={{
+            name: "Primeiro nome",
+            email: "Seu e-mail",
+          }}
+        />
+      </FooterContainer>
 
-          <FooterContainer>
-            {/* Desktop view */}
-            <ul class="hidden sm:flex flex-row gap-20">
-              {sections.map((section) => (
-                <li>
-                  <div>
-                    <Text variant="heading-3" tone="default-inverse">
-                      {section.label}
-                    </Text>
+      <FooterContainer class="bg-white w-full">
+        {/* Desktop view */}
+        <ul class="hidden sm:flex flex-row gap-32 mx-5">
+          {links.map((link) => (
+            <li>
+              <div>
+                <Text variant="heading-3" tone="default" class="uppercase">
+                  {link.title}
+                </Text>
 
-                    <ul
-                      class={`flex ${
-                        isIcon(section.children[0]) ? "flex-row" : "flex-col"
-                      } gap-2 pt-2 flex-wrap`}
-                    >
-                      {section.children.map((item) => (
-                        <li>
-                          <SectionItem item={item} />
-                        </li>
-                      ))}
-                    </ul>
-                  </div>
-                </li>
-              ))}
-            </ul>
-
-            {/* Mobile view */}
-            <ul class="flex flex-col sm:hidden sm:flex-row gap-4">
-              {sections.map((section) => (
-                <li>
-                  <Text variant="body" tone="default-inverse">
-                    <details>
-                      <summary>
-                        {section.label}
-                      </summary>
-
-                      <ul
-                        class={`flex ${
-                          isIcon(section.children[0]) ? "flex-row" : "flex-col"
-                        } gap-2 px-2 pt-2`}
-                      >
-                        {section.children.map((item) => (
-                          <li>
-                            <SectionItem item={item} />
-                          </li>
-                        ))}
-                      </ul>
-                    </details>
+                <ul
+                  class={`flex ${
+                    isIcon(link.items[0]) ? "flex-col" : "flex-col"
+                  } gap-3 pt-2 flex-wrap`}
+                >
+                  {link.items.map((item) => (
+                    <li class="flex flex-row gap-2">
+                      <SectionItem item={item} />
+                      <Text class="hover:font-black">{item.title}</Text>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </li>
+          ))}
+          <li>
+            <div>
+              <Text variant="heading-3">Dúvidas ou Suporte:</Text>
+              <ul>
+                <li class="flex flex-col mt-10">
+                  <Text variant="heading-3" class="font-black">
+                    Regiões Metropolitanas
                   </Text>
+                  {support.phoneNumber}
                 </li>
-              ))}
-            </ul>
-          </FooterContainer>
-        </Container>
-      </div>
+                <li class="flex flex-col my-10">
+                  <Text variant="heading-3" class="font-black">
+                    Horários de atendimento:
+                  </Text>
+                  {support.openingHours.map((hours) => <Text>{hours}</Text>)}
+                </li>
+              </ul>
+              <ul class="flex flex-col">
+                <Text variant="heading-3" class="font-bold">
+                  Siga-nos nas redes sociais
+                </Text>
+                <div class="flex flex-row">
+                  {social.map((item) => (
+                    <li class="gap-2">
+                      <Icon
+                        media="(min-width: 768px)"
+                        id={item.icon}
+                        width={30}
+                        height={25}
+                        strokeWidth={1.5}
+                        class="text-red-600"
+                      />
+                    </li>
+                  ))}
+                </div>
+              </ul>
+            </div>
+          </li>
+        </ul>
 
-      <div>
-        <Container class="w-full">
-          <FooterContainer class="flex justify-between w-full">
-            <Text
-              class="flex items-center gap-1"
-              variant="body"
-              tone="default-inverse"
+        {/* Mobile view */}
+        <ul class="flex flex-col sm:hidden sm:flex-row gap-4">
+          {links.map((link) => (
+            <li>
+              <Text variant="body" tone="default-inverse">
+                <details>
+                  <summary>
+                    {link.title}
+                  </summary>
+
+                  <ul
+                    class={`flex ${
+                      isIcon(link.items[0]) ? "flex-row" : "flex-col"
+                    } gap-2 px-2 pt-2`}
+                  >
+                    {link.items.map((item) => (
+                      <li>
+                        <SectionItem item={item} />
+                      </li>
+                    ))}
+                  </ul>
+                </details>
+              </Text>
+            </li>
+          ))}
+        </ul>
+      </FooterContainer>
+
+      <FooterContainer class="flex justify-between w-full bg-white gap-20">
+        <Image
+          src={copyright.logo}
+          alt="logo"
+          width={200}
+          height={100}
+          class="object-fill object-center mx-5"
+        />
+        <div>
+          {copyright.lines}
+        </div>
+        <div>
+          <h1>Desenvolvido por mim mesmo</h1>
+        </div>
+      </FooterContainer>
+
+      <FooterContainer class="flex justify-between w-full">
+        <Text
+          class="flex items-center gap-1"
+          variant="body"
+          tone="default-inverse"
+        >
+          Powered by{" "}
+          <a
+            href="https://www.deco.cx"
+            aria-label="powered by https://www.deco.cx"
+          >
+            <Icon id="Deco" height={20} width={60} strokeWidth={0.01} />
+          </a>
+        </Text>
+
+        <ul class="flex items-center justify-center gap-2">
+          <li>
+            <a
+              href="https://www.instagram.com/deco.cx"
+              target="_blank"
+              rel="noopener noreferrer"
+              aria-label="Instagram logo"
             >
-              Powered by{" "}
-              <a
-                href="https://www.deco.cx"
-                aria-label="powered by https://www.deco.cx"
-              >
-                <Icon id="Deco" height={20} width={60} strokeWidth={0.01} />
-              </a>
-            </Text>
-
-            <ul class="flex items-center justify-center gap-2">
-              <li>
-                <a
-                  href="https://www.instagram.com/deco.cx"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  aria-label="Instagram logo"
-                >
-                  <Icon
-                    class="text-default-inverse"
-                    width={32}
-                    height={32}
-                    id="Instagram"
-                    strokeWidth={1}
-                  />
-                </a>
-              </li>
-              <li>
-                <a
-                  href="http://www.deco.cx/discord"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  aria-label="Discord logo"
-                >
-                  <Icon
-                    class="text-default-inverse"
-                    width={32}
-                    height={32}
-                    id="Discord"
-                    strokeWidth={5}
-                  />
-                </a>
-              </li>
-            </ul>
-          </FooterContainer>
-        </Container>
-      </div>
+              <Icon
+                class="text-default-inverse"
+                width={32}
+                height={32}
+                id="Instagram"
+                strokeWidth={1}
+              />
+            </a>
+          </li>
+          <li>
+            <a
+              href="http://www.deco.cx/discord"
+              target="_blank"
+              rel="noopener noreferrer"
+              aria-label="Discord logo"
+            >
+              <Icon
+                class="text-default-inverse"
+                width={32}
+                height={32}
+                id="Discord"
+                strokeWidth={5}
+              />
+            </a>
+          </li>
+        </ul>
+      </FooterContainer>
     </footer>
   );
 }
